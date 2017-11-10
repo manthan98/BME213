0d752695429371b645c8f7279ee853e6799a37cf
Manthan Shah <manthan711@gmail.com>
2017-11-10 09:20:25 -0500

    Create a4.R

---
+++ Assignment 4/a4.R
@@ -0,0 +1,6 @@
+binom.test(10, 12, 0.5, "two.sided") # Q1
+dbinom(0, size=2, prob=0.38) * 87
+dbinom(1, size=2, prob=0.38) * 87
+dbinom(2, size=2, prob=0.38) * 87
+
+pchisq(14.92, 1, lower.tail = FALSE)
